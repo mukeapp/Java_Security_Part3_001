4d27ddfbb89394e462bb8e8dd96c77e3c8af2ae0
Franck Mukendi <franck.mukendi@gmail.com>
2020-10-09 16:10:45 -0400

    Update SecurityConfig.java

--- src/main/java/com/javainuse/config/SecurityConfig.java
+++ src/main/java/com/javainuse/config/SecurityConfig.java
@@ -30,3 +30,34 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	}
 
 }
+
+/*
+Error : There is no PasswordEncoder mapped for the id “null”
+Solution : https://www.yawintutor.com/illegalargumentexception-there-is-no-passwordencoder-mapped-for-the-id-null/
+
+Solution 1
+The default PasswordEncoder is NoOpPasswordEncoder, which deals for plain text passwords until Spring Security 5.0. The default in Spring Security 5 is changed to DelegatingPasswordEncoder that required Password Storage Format.
+
+The password storage format is {encrypt}password. Add {noop}, before the password. {noop} informs that password is a plain text, no encryption is used.
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.withUser("user").password("{noop}password").roles("USER")
+			.and()
+			.withUser("admin").password("{noop}password").roles("ADMIN");
+	}
+Solution 2
+In the Password Encoder, set the default PasswordEncoder NoOpPasswordEncoder. The example below shows the default Password Encoder with NoOpPasswordEncoder.
+
+The NoOpPasswordEncoder class is deprecated. This solution should not be used in production environment
+
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+			.passwordEncoder(NoOpPasswordEncoder.getInstance())
+			.withUser("user").password("password").roles("USER")
+			.and()
+			.withUser("admin").password("password").roles("ADMIN");
+	}
+ */
